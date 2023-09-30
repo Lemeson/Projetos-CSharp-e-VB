93ec66977f7549a2a8ba12ce4f70f04386ca183e
lemeson <lemeson93@gmail.com>
2023-09-30 14:43:55 -0300

    first commit

---
+++ GarbageCollection/Demo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoTestCSharp
+{
+    class Demo
+    {
+        public static long Instances;
+
+        public Demo()
+        {
+            Instances++;
+        }
+
+        ~Demo()
+        {
+            Instances--;
+        }
+    }
+}
